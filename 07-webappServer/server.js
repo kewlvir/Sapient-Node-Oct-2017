ae31cf4c241fcc6e769b04a7c8a48711da1822f7
Magesh <tkmagesh77@gmail.com>
2017-10-08 12:18:38 +0530

    calculator - post

--- 07-webappServer/server.js
+++ 07-webappServer/server.js
@@ -21,7 +21,7 @@ var server = http.createServer(function(req, res){
 			return;
 		}
 		fs.createReadStream(resourcePath).pipe(res);
-	} else if (reqObj.pathname === '/calculator'){
+	} else if (reqObj.pathname === '/calculator' && req.method === 'GET'){
 		var reqData = querystring.parse(reqObj.query);
 		var op = reqData.op,
 			n1 = parseInt(reqData.n1, 10),
@@ -29,6 +29,20 @@ var server = http.createServer(function(req, res){
 		var result = calculator[op](n1, n2);
 		res.write(result.toString());
 		res.end();
+	} else if (reqObj.pathname === '/calculator' && req.method === 'POST'){
+		var rawData = '';
+		req.on('data', function(chunk){
+			rawData += chunk;
+		});
+		req.on('end', function(){
+			var reqData = querystring.parse(rawData);
+			var op = reqData.op,
+				n1 = parseInt(reqData.n1, 10),
+				n2 = parseInt(reqData.n2, 10);
+			var result = calculator[op](n1, n2);
+			res.write(result.toString());
+			res.end();
+		});
 	} else {
 		res.statusCode = 404;
 		res.end();
